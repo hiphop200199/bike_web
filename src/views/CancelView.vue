15c6be84575ceb6b6afaf931d06f77ca8d704751
hiphop200199 <hiphop200199@gmail.com>
2025-08-12 00:04:41 +0800

    cancel img

--- src/views/CancelView.vue
+++ src/views/CancelView.vue
@@ -5,7 +5,6 @@
       <h3>搜尋</h3>
       <p>尋找最喜歡的那一本書</p>
       <div id="pic-and-link">
-        <img src="../assets/blonde-1866951_1280.jpg" alt="" />
         <router-link to="/list">連結</router-link>
       </div>
     </section>
--- src/views/SuccessView.vue
+++ src/views/SuccessView.vue
@@ -1,21 +1,19 @@
 <script setup>
-import LoadingAnimation from '@/components/LoadingAnimation.vue'
+/* import LoadingAnimation from '@/components/LoadingAnimation.vue'
 import { useCartStore } from '@/stores/store'
 import { useRoute } from 'vue-router'
 const route = useRoute()
 const sessionId = route.query.session_id
 const cartStore = useCartStore()
-cartStore.checkoutSuccess(sessionId)
+cartStore.checkoutSuccess(sessionId) */
 </script>
 <template>
   <div id="homepage-container">
-    <loading-animation v-if="cartStore.isLoading"></loading-animation>
-    <h1 class="checkout-result-title" v-if="!cartStore.isLoading">結帳成功!</h1>
-    <section id="to-search" v-if="!cartStore.isLoading">
+    <h1 class="checkout-result-title">結帳成功!</h1>
+    <section id="to-search">
       <h3>搜尋</h3>
       <p>尋找最喜歡的那一本書</p>
       <div id="pic-and-link">
-        <img src="../assets/blonde-1866951_1280.jpg" alt="" />
         <router-link to="/list">連結</router-link>
       </div>
     </section>
